5d76782d5063c9f783aa0e9b21b3cafaaa517c3a
Kedres <kevin150598@gmail.com>
2020-06-22 14:44:07 -0600

    Se agraga utilidad para generar direcciones fisicas aleatoriamente

---
+++ util/MAC_generator.py
@@ -0,0 +1,17 @@
+'''
+Utilidad para generar las direcciones MAC necesarias en las conexiones
+'''
+from random import choice
+
+valores = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+           'A', 'B', 'C', 'D', 'E', 'F']
+extension = 10
+
+def generate():
+
+    new_address = ''
+
+    for i in range(extension):
+        new_address += choice(valores)
+
+    print(new_address)
