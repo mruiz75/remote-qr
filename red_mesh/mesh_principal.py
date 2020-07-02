95f84402bc6821215f07053918adb95de360ac86
Kedres <kevin150598@gmail.com>
2020-07-02 15:57:18 -0600

    Se termino la funcionalidad de salir de la red mesh y se comento el codigo

--- red_mesh/mesh_principal.py
+++ red_mesh/mesh_principal.py
@@ -1,10 +1,10 @@
 import socket
-from _thread import start_new_thread
 from threading import Thread
 
 import MAC_generator
 import manejo_nodos as mn
 
+#Clase que funciona como un servidor de registro a la red mesh
 class MeshPrincipal:
 
     def __init__(self):
@@ -14,6 +14,7 @@ class MeshPrincipal:
         self.datosNodo = mn.CrearNodo(ip, mac.generate(), puerto)
         self.nodosConectados = mn.ListaNodos()
 
+    #Funcion que crear un nodo nuevo con el ip y puerto del solicitante y una mac única
     def crear_nodo_registro(self, direccion_cliente):
         generador = MAC_generator.MACGenerator()
         generarMAC = True
@@ -34,6 +35,7 @@ class MeshPrincipal:
         nodo_cliente = mn.CrearNodo(direccion_cliente[0], mac_cliente, int(direccion_cliente[1]))
         return nodo_cliente
 
+    #Funcion que informa a toda la red del nuevo cliente
     def agregar_cliente(self, cliente, direccion):
         nodoCliente = self.crear_nodo_registro(direccion)
         string_datos = nodoCliente.to_string()
@@ -53,7 +55,22 @@ class MeshPrincipal:
         self.nodosConectados.agregar_nodo(nodoCliente)
         print("Agregado el nodo %s:%s" % (nodoCliente.get_ip(), str(nodoCliente.get_puerto())))
 
+    #Funcion que desconecta al cliente de la red mesh
+    def borrar_cliente(self, macNodo):
+        for nodo in self.nodosConectados.get_lista():
+            try:
+                conexion = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                conexion.connect((nodo.get_ip(), nodo.get_puerto()))
+                conexion.send(('DEL ' + macNodo).encode('utf-8'))
+                conexion.close()
+            except:
+                pass
+        indiceNodo = self.nodosConectados.buscar_nodo(macNodo)
+        datosBorrado = self.nodosConectados.eliminar_nodo(indiceNodo)
+        print('Se elimino el cliente: ' + datosBorrado.get_mac())
 
+
+    #Funcion principal que esta en la escucha de conexiones o desconexiones en la red
     def registro(self):
         # Valores del socket
         dataConection = (self.datosNodo.get_ip(), self.datosNodo.get_puerto())
@@ -69,49 +86,23 @@ class MeshPrincipal:
         while True:
             cliente, direccion = socketRegistro.accept()
             print("Conexion establecida con %s:%s" % (direccion[0], direccion[1]))
-            hilo_nuevo_registro = Thread(target=self.agregar_cliente, args=(cliente
+            solicitud = cliente.recv(20).decode('utf-8')
+            solicitud = solicitud.split()
+            if solicitud[0] == 'CONNECT':
+                hilo_nuevo_registro = Thread(target=self.agregar_cliente, args=(cliente
                                                                             , direccion))
-            hilo_nuevo_registro.start()
-            hilo_nuevo_registro.join()
+                hilo_nuevo_registro.start()
+                hilo_nuevo_registro.join()
+            else:
+                cliente.close()
+                hilo_borrar_registro = Thread(target=self.borrar_cliente, args=(solicitud[1],))
+
+                hilo_borrar_registro.start()
+                hilo_borrar_registro.join()
+
 
 
 
 if __name__ == "__main__":
     mesh = MeshPrincipal()
-    #p = mesh.crear_nodo_registro(['0.0.0.0', 1616])
-    #print(p.get_ip())
-    #print(p.get_mac())
-    #print(p.get_puerto())
-
-    #p = mesh.agregar_cliente(0, ['0.0.0.0', 1616])
     mesh.registro()
-
-'''
-ip = "0.0.0.0"
-puerto = 1505
-dataConection = (ip, puerto)
-
-#Creamos el servidor.
-#socket.AF_INET para indicar que utilizaremos Ipv4
-#socket.SOCK_STREAM para utilizar TCP/IP (no udp)
-socketServidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-socketServidor.bind(dataConection) #Asignamos los valores del servidor
-socketServidor.listen() #Asignamos el número máximo de conexiones
-
-print("Esperando conexiones en %s:%s" %(ip, puerto))
-cliente, direccion = socketServidor.accept()
-print("Conexion establecida con %s:%s" %(direccion[0], direccion[1]))
-
-#Bucle de escucha. En él indicamos la forma de actuar al recibir las tramas del cliente
-while True:
-    datos = cliente.recv(1024).decode('utf-8') #El número indica el número maximo de bytes
-    if datos == "exit":
-        cliente.send("exit".encode('utf-8'))
-        break
-    print("RECIBIDO: %s" %datos)
-    cliente.sendall("-- Recibido --".encode('utf-8'))
-
-print("------- CONEXIÓN CERRADA ---------")
-socketServidor.close()
-'''
